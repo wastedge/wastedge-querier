fd18bb8bd063779d33bab78204993bde497f61d8
Pieter van Ginkel <pginkel@gmt.nl>
2016-03-14 10:29:35 +0100

    Created version 1.1.0.0.

--- WastedgeQuerier/Properties/AssemblyInfo.cs
+++ WastedgeQuerier/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("97c2349d-eb63-40f8-8b33-aef4d50b52e7")]
 
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
