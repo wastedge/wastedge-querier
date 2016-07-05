70019be1dd005afdf687c9ba28b6c851cdbc6011
Ivan Zhu <IZhu@gmt.nl>
2016-07-05 13:45:37 +1000

    Revert "Fixed: Should hide the extension in save/rename files."
    This reverts commit 7ddde0363291226682def54eb060db445d9ffcce.

--- WastedgeQuerier/SaveForm.cs
+++ WastedgeQuerier/SaveForm.cs
@@ -14,15 +14,13 @@ namespace WastedgeQuerier
     public partial class SaveForm : SystemEx.Windows.Forms.Form
     {
         private string _path;
-        private string _extension;
 
         public string Path
         {
-            get { return IOPath.Combine(_path, _nameTextBox.Text + _extension); }
+            get { return IOPath.Combine(_path, _nameTextBox.Text); }
             set
             {
-                _nameTextBox.Text = IOPath.GetFileNameWithoutExtension(value);
-                _extension = IOPath.GetExtension(value);
+                _nameTextBox.Text = IOPath.GetFileName(value);
                 _path = IOPath.GetDirectoryName(value);
             }
         }
